f6a632732ef1c1cafec79006605cfaa0359a0085
jaythamzz <161520878+jaythamzz@users.noreply.github.com>
2025-03-17 16:31:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Flowers for you paige').split('')
+    const titles = ('Flowers for you my love Tricia Javier').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
